96333851bc08916497a4a583001f26b5d894195e
Prajina <prajina.bhattarai023@apexcollege.edu.np>
2025-06-30 12:21:06 +0545

    restrutre backend

--- backend/auth/apps.py
+++ backend/auth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SikhaiConfig(AppConfig):
+class AuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SIKHAI'
+    name = 'auth'
