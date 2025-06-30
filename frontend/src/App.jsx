06f8487105821c9484cacf7d5f2dbf80967a282c
Shreyan Parajuli <shreyanparajuli@sysnefo.com>
2025-06-30 18:31:59 +0545

    add neccesary files structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,18 @@ import Register from "./Auth/Register";
 import {BrowserRouter as Router, Route, Routes} from "react-router"
 import LandingPage from "./LandingPage/LandingPage"
 import DashboardLayout from "./Dashboard/DashboardLayout";
+import NoteView from "./Notes/NoteView";
+import TodoView from "./Todo/TodoView"
+import Whiteboard from "./Whiteboard/Whiteboard";
 
 const App = () => {
     return(
         <Router>
             <Routes>
                 <Route path="/dashboard" element={<DashboardLayout />} />
+                <Route path="/notes" element={<DashboardLayout><NoteView /></DashboardLayout>} />
+                <Route path="/todos" element={<DashboardLayout><TodoView /></DashboardLayout>} />
+                <Route path="/whiteboard" element={<DashboardLayout><Whiteboard /></DashboardLayout>} />
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
--- frontend/src/Dashboard/Sidebar.jsx
+++ frontend/src/Dashboard/Sidebar.jsx
@@ -1,20 +1,23 @@
 import { HomeIcon, CircleCheckBig, Notebook, NotebookPen, Presentation, Pencil, BookCheck, Bolt, LogOut, Menu } from "lucide-react";
-
+import { useNavigate } from "react-router";
+import { useState } from "react";
 
 
 export default function Sidebar(props){
+    const navigate = useNavigate()
+    const [expanded, setExpanded] = useState(false)
     return(
         <>
         <div className="flex flex-col bg-white h-[100vh] justify-between gap-4 p-5 w-16 rounded-xl ">
             <div className="flex flex-col gap-2 items-center">
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><Menu /></button></span>
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><HomeIcon /></button></span>
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><CircleCheckBig /></button></span>
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><NotebookPen /></button></span>
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><Notebook /></button></span>
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><Presentation /></button></span>
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><Pencil /></button></span>
-                <span><button className="hover:bg-sidebar-items p-2 rounded-md"><BookCheck /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => setExpanded(prev => !prev)}><Menu /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => navigate('/dashboard')}><HomeIcon /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => navigate('/todos')}><CircleCheckBig /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => navigate('/notes')}><NotebookPen /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => navigate('/')}><Notebook /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => navigate('/whiteboard')}><Presentation /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => navigate('/')}><Pencil /></button></span>
+                <span><button className="hover:bg-sidebar-items p-2 rounded-md" onClick={() => navigate('/')}><BookCheck /></button></span>
             </div>
 
             <div className="flex flex-col gap-6 items-center">
