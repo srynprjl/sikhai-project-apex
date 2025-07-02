d05ec0ba9b7689203324cacad5c3adfb10a2c9f5
Shreyan Parajuli <shreyanparajuli@sysnefo.com>
2025-07-02 18:51:24 +0545

    fix accordion

--- frontend/src/Todo/components/Tasks.jsx
+++ frontend/src/Todo/components/Tasks.jsx
@@ -22,11 +22,11 @@ export default function Tasks(props) {
                 onClick={handleComplete}
                 className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded-full  border border-gray-300 checked:bg-blue-600 "
               />
-              <div className={completed ? "line-through decoration-2" : ""}>
+              <div onClick={() => setExpanded(prev => !prev)} className={completed ? "line-through decoration-2" : ""}>
                 {props.name}
               </div>
             </div>
-            <div onClick={() => setExpanded(prev => !prev)} onMouseOver={() => handleHover("enter")} onMouseLeave={() => handleHover("leave")}>{hover ? <div className="flex gap-1"><button className="bg-green-400 p-1 rounded-md"><Pen size={16}/></button> <button className="bg-red-500 p-1 rounded-md"><Trash size={16}/></button></div> :<input type="date" value={props.dueDate} />}</div>
+            <div  onMouseOver={() => handleHover("enter")} onMouseLeave={() => handleHover("leave")}>{hover ? <div className="flex gap-1"><button className="bg-green-400 p-1 rounded-md"><Pen size={16}/></button> <button className="bg-red-500 p-1 rounded-md"><Trash size={16}/></button></div> :<input type="date" value={props.dueDate} />}</div>
           </div>
           {
             expanded ? <div className="text-sm ">
