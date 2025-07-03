2cbf26a942f715cfa52576161a84ca849bd43d84
Shreyan Parajuli <shreyanparajuli@sysnefo.com>
2025-07-03 08:18:58 +0545

    moved all the pages into pages directory

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,15 +1,32 @@
-import Login from "./Auth/Login";
-import Register from "./Auth/Register";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
-import LandingPage from "./LandingPage/LandingPage";
-import DashboardLayout from "./Dashboard/DashboardLayout";
-import NoteView from "./Notes/NoteView";
-import TodoView from "./Todo/TodoView";
-import Whiteboard from "./Whiteboard/Whiteboard";
-import TodoCreate from "./Todo/TodoCreate";
-import NoteCreate from "./Notes/NoteCreate";
-import NotePage from "./Notes/components/NotePage";
-import PublicNotesView from "./Notes/PublicNotesView";
+
+// Auth Pages
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+// Landing Page
+import LandingPage from "./pages/LandingPage/LandingPage";
+
+// Dashboard
+import DashboardLayout from "./pages/Dashboard/DashboardLayout";
+
+
+//whiteboard
+import Whiteboard from "./pages/Whiteboard/Whiteboard";
+
+// todo
+import TodoView from "./pages/Todo/TodoView";
+import TodoCreate from "./pages/Todo/TodoCreate";
+
+//notes
+import NoteView from "./pages/Notes/NoteView";
+import NoteCreate from "./pages/Notes/NoteCreate";
+import NotePage from "./pages/Notes/components/NotePage";
+import PublicNotesView from "./pages/Notes/PublicNotesView";
+
+// tutors
+
+// exams
 
 const AppRouter = () => {
   return (
