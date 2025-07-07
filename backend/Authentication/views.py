97538172fe326883167fd0eae40a8e9143395c6a
Shreyan Parajuli <shreyanparajuli@sysnefo.com>
2025-07-07 08:15:20 +0545

    add admin essentials

--- backend/Authentication/views.py
+++ backend/Authentication/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from .models import CustomUser
-from rest_framework import generics , viewsets
+from rest_framework import generics
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import  AllowAny, IsAuthenticated
 
 #Viewssssssssssss
 class CreateUserView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class UsernameView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response({'username': request.user.username, 'first_name': request.user.first_name, 'last_name': request.user.last_name, 'email': request.user.email})
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path , include
-from Authentication.views import CreateUserView
+from Authentication.views import CreateUserView, UsernameView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from whiteboard.views import board_view
@@ -10,6 +10,7 @@ from whiteboard.views import board_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/user/info/", UsernameView.as_view(), name="username"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api/board/", board_view, name="board"),
