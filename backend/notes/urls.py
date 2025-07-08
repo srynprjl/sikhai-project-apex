fd938ea7f21007c91d011c90ad058d0f4ad9db02
Prajina <prajina.bhattarai023@apexcollege.edu.np>
2025-07-08 11:27:02 +0545

    add notes delete and list view

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
+    path("notes/<int:pk>/", views.NoteDetail.as_view(), name="note-list"),   
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
+    path("notes/update/<int:pk>/", views.NoteUpdate.as_view(), name="update-note"),
 ]
