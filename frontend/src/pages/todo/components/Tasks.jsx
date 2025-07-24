8e13467b1bb027f146bf3de96ecf3e5ef9fb08b4
Rakshya Dhakal <rakshya.dhakal023@gmail.com>
2025-07-24 12:33:01 +0545

    add tasks page with api

--- frontend/src/pages/todo/components/Tasks.jsx
+++ frontend/src/pages/todo/components/Tasks.jsx
@@ -1,24 +1,28 @@
 import { Edit, Pen, Trash } from "lucide-react";
 import { useState } from "react";
+import api from "../../../api";
 
 export default function Tasks(props) {
     
-  const [completed, setCompleted] = useState(false);
+  const [completed, setCompleted] = useState(props.completed);
   const [expanded, setExpanded] = useState(false);
-  function handleComplete() {
+
+  async function handleComplete(id) {
     setCompleted(prev => !prev)
+    const res = await api.patch(`/api/tasks/${id}/toggle-complete/`, {'completed': !completed})
+    console.log(res)
   }
 
   return (
     <>
-      
         <div className="py-2 px-2 flex gap-5 flex-col bg-dark-tertiary w-full ">
           <div className="flex justify-between items-center">
             <div className="flex gap-2 items-center">
               <input
                 type="checkbox"
-                value={completed ? "on" : "off"}
-                onClick={handleComplete}
+                // value={completed ? "on" : "off"}
+                checked={completed}
+                onClick={() => {handleComplete(props.id)}}
                 className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded-full border border-accent checked:bg-accent "
               />
               <div onClick={() => setExpanded(prev => !prev)} className={completed ? "line-through decoration-2" : ""}>
